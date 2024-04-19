17590425b33c7ecc50d4f7eb61b8b90b6a2f8039
Vinay Jain <87294500+vinay0101@users.noreply.github.com>
2024-04-20 02:51:13 +0530

    Add second principle

--- SOLID.java
+++ SOLID.java
@@ -1,4 +1,4 @@
-// Single Responsibility principle - A class should have 1 reason to change
+// 1. Single Responsibility principle - A class should have 1 reason to change
 
 class Marker{
     String name;
@@ -14,7 +14,7 @@ class Marker{
     }
 }
 
-// Does not follow - There are 3 reasons to change in this case - if logic of any of the 3 methods - calculateTotal, printInvoice, saveToDB changes
+// Does not follow the principle - There are 3 reasons to change in this case - if logic of any of the 3 methods - calculateTotal, printInvoice, saveToDB changes
 class Invoice {
     private Marker marker;
     private int quantity;
@@ -72,3 +72,47 @@ class InvoicePrinter {
         //print the Invoice
     }
 }
+
+// 2. Open/Closed Principle - Open for extension but closed for Modification
+
+// If there is a request to add new functionality for saving to File - then need to add new method in class
+class InvoiceDao {
+    Invoice invoice;
+
+    public InvoiceDao(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public void saveToDB(){
+        // Save the data into DB
+    }
+    public void saveToFile(String fileName){
+        // Save invoice in the File with the given name
+    }
+}
+
+// Solution -> Use interface to allow it for extension but not able to modidy
+interface InvoiceDao{
+    public void save(Invoice invoice);
+}
+
+class SQLDatabaseInvoiceDao implements InvoiceDao{
+    @Override
+    public void save(){
+        // Save the data into SQL DB
+    }
+}
+class FileInvoiceDao implements InvoiceDao{
+    @Override
+    public void save(){
+        // Save to File
+    }
+}
+
+class NoSQLDatabaseInvoiceDao implements InvoiceDao{
+    @Override
+    public void save(){
+        // Save the data into No-SQL DB
+    }
+}
+
